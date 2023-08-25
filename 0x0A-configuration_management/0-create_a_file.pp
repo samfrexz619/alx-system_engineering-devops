b563ed819e5413615ccc67859b04a0729772d834
samuel Damilola <samfrexz69@gmail.com>
2023-08-25 04:28:07 -0700

    upd file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
 # Create a file using puppet
 
-file { '/tmp/school:
-  ensure  => file,
-  path    => '/tmp/school',
+file { '/tmp/school':
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
